8a3aac5c8fe8a7528c92654942c4f5a45549752b
guykamty <guykamty@gmail.com>
2024-12-20 15:38:10 +0700

    lab11_1_complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,25 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    cout << "Press Enter 3 times to reveal your future." << endl;
+    
+    string input;
+    int count = 0;
+    while(count < 3) {
+        getline(cin, input);
+        count++;
+    }
+    
+    srand(time(0));
+    int gradeIndex = rand() % 9;
+
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+
+    cout << "You will get " << grades[gradeIndex] << " in this 261102." << endl;
+
+    return 0;
+}
